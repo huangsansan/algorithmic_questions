8944d239019ec5fbc5e1e511f5ddc6c2c7ef2c1d
huangsan <372506589@qq.com>
2021-08-23 16:43:37 +0800

    feat：线程池部分

--- src/BK2020/M08/D06/BK_JavaHeapSpace.java
+++ src/BK2020/M08/D06/BK_JavaHeapSpace.java
@@ -1,12 +1,20 @@
 package BK2020.M08.D06;
 
+import java.util.Random;
+
 /**
  * Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
  */
 public class BK_JavaHeapSpace {
 
     public static void main(String[] args) {
-        Byte[] bytes = new Byte[6 * 1024 * 1024];
+//        System.gc();
+//        Byte[] bytes = new Byte[6 * 1024 * 1024];
+        String a = "aaa";
+        while (true) {
+            a += a + new Random().nextInt(7777777) + new Random().nextInt(66666666);
+            a.intern();
+        }
     }
 
 }
