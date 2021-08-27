85ef9c70ca3b93983d8a355b9a445eb22a5f8e1d
huangsan <372506589@qq.com>
2021-08-27 18:25:11 +0800

    feat：netty进阶

---
+++ src/main/java/com/hhb/netty/c2/LengthFieldClient.java
@@ -0,0 +1,49 @@
+package com.hhb.netty.c2;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetSocketAddress;
+import java.util.Random;
+
+/**
+ * @description:
+ * @Author: huangsan
+ * @Date: 2021/8/27 下午5:50
+ */
+@Slf4j
+public class LengthFieldClient {
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup(2);
+        new Bootstrap()
+                //2、添加EventLoop
+                .group(group)
+                //3、选择客户端channel实现
+                .channel(NioSocketChannel.class)
+                //4、添加处理器handler
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nc) throws Exception {
+                        nc.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                        nc.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                ByteBuf buffer = ctx.alloc().buffer();
+                                buffer.writeShort(6);
+                                buffer.writeBytes("123456789".getBytes());
+                                ctx.writeAndFlush(buffer);
+                                super.channelActive(ctx);
+                            }
+                        });
+                    }
+                }).connect(new InetSocketAddress("localhost", 8080)).sync().channel();
+    }
+}
---
+++ src/main/java/com/hhb/netty/c2/LengthFieldServe.java
@@ -0,0 +1,45 @@
+package com.hhb.netty.c2;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @description:
+ * @Author: huangsan
+ * @Date: 2021/8/27 下午5:51
+ */
+@Slf4j
+public class LengthFieldServe {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup(), new NioEventLoopGroup(2))
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nc) throws Exception {
+                        nc.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                        nc.pipeline().addLast(new LengthFieldBasedFrameDecoder(100,0,2,3,2) {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf byteBuf = (ByteBuf) msg;
+                                log.info("ByteBuf:{}", byteBuf);
+                                super.channelRead(ctx, msg);
+                            }
+                        });
+                        nc.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                    }
+                })
+                //7绑定端口
+                .bind(8080);
+    }
+}
